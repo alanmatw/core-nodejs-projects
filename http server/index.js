3151ec227899883313ac69aaef8ca3d05f3db262
alanmatw <alanmatw@gmail.com>
2020-08-20 22:26:16 +0530

    feat(http server): workarounds

--- http server/index.js
+++ http server/index.js
@@ -1,14 +1,16 @@
+// import package
 const http = require('http')
 
 // create server object
-const requestListner = (req, res) => {
+const requestListner = function(req, res) {
   res.writeHead(200) // response header
-  res.end('Hello World!') // respose message
+  res.end('Hello World!') // response end
 }
 
+// create server
 const server = http.createServer(requestListner)
 
 // server listens in port 8080
-server.listen(8080, () => {
+server.listen(8080, function() {
     console.log('Server running on http://localhost:8080')
 })
