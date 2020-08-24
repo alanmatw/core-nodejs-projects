b004577ee26895201a078e24d1361b92670bcc49
alanmatw <alanmatw@gmail.com>
2020-08-24 07:56:46 +0530

    feat(http rest server): tweaks

--- http rest server/index.js
+++ http rest server/index.js
@@ -8,12 +8,37 @@ const port = 8080;
 // create server
 const server = http.createServer((req, res) => {
 
-  if(req.url == '/' && req.method == 'GET') {
-    res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
-    res.write(JSON.stringify(Users.getUsers()));
-    res.end();
+  if(req.method == 'GET') {
+    handleGet(req, res);
+  } else if(req.method == 'POST') {
+    handlePost(req, res);
+  } else if(req.method == 'PUT') {
+    handlePut(req, res);
+  } else if(req.method == 'DELETE') {
+    handleDelete(req, res);
   }
 
 }).listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
+
+// fn to handle GET
+handleGet = (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+  return res.end(JSON.stringify(Users.getUsers()));
+}
+
+// fn to handle POST
+handlePost = (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+}
+
+// fn to handle PUT
+handlePut = (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+}
+
+// fn to handle DELETE
+handleDelete = (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+}
