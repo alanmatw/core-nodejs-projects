2f1f44d0313ae4550a0341943cf96096b7ad1d51
agin19 <49949663+agin19@users.noreply.github.com>
2020-10-01 22:12:38 +0530

    Update client.js

--- tcp client-server/client.js
+++ tcp client-server/client.js
@@ -1,16 +1,22 @@
+// import net package
 var net = require('net');
 
+// create socket
 var client = new net.Socket();
+
+// connect to server
 client.connect(1337, '127.0.0.1', function() {
 	console.log('Connected');
 	client.write('Hello, server! Love, Client.');
 });
 
+// receive from server
 client.on('data', function(data) {
 	console.log('Received: ' + data);
 	client.destroy(); // kill client after server's response
 });
 
+// close connection
 client.on('close', function() {
 	console.log('Connection closed');
 });
