34e21f9c5d1db641d36ad86f7f7d48181a4287cd
agin19 <49949663+agin19@users.noreply.github.com>
2020-10-01 22:09:27 +0530

    Update server.js

--- tcp client-server/server.js
+++ tcp client-server/server.js
@@ -1,8 +1,12 @@
+// import net package
 var net = require('net');
 
+// create server
 var server = net.createServer(function(socket) {
 	socket.write('Echo server\r\n');
 	socket.pipe(socket);
 });
 
+
+// server listening at port 1337
 server.listen(1337, '127.0.0.1');
